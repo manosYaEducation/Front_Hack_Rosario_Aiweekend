88c45039ade2e38a0e433b75d5d94f1a96949ea1
tdelacarrera <202549429+tdelacarrera@users.noreply.github.com>
2025-10-08 17:34:29 -0300

    Se ha agregado el campo pitch en el formulario de editar proyecto y se ha actualizado el label y placeholder del campo pitch en el formulario de crear proyecto

--- views/project-create.php
+++ views/project-create.php
@@ -22,8 +22,8 @@
                     <input type="text" id="description" name="description" placeholder="Ingresa la descripción" required>
                 </div>
                 <div class="form-group">
-                    <label for="pitch">Subir pitch</label>
-                    <input type="text" id="pitch" name="pitch">
+                    <label for="pitch">Enlace pitch</label>
+                    <input type="text" id="pitch" name="pitch" placeholder="Ingresa el enlace de tu pitch">
                 </div>
                 <div class="form-group">
                     <label for="image">Subir imagen</label>
